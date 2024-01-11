ea5a65749b2d4fdc5289f26d9afa1607da2bafba
unknown <kyllianhouche@gmail.comgit config --global user.name Kyllian-11$ git config --global user.email kyllianhouche@gmail.com>
2024-01-11 15:19:01 +0100

    upload

--- card.php
+++ card.php
@@ -3,7 +3,7 @@
     foreach ($chocolat as $value) {
         echo '<div class="card"
                     style=" width:30%; background-color: #2A0045">
-                <img src="image/' . $value['img'] . '.png" class="card-img-top" alt="">
+                <img src="image/' . $value['img'] . '.png" class="card-img-top" alt=""> 
                 <div class="card-body">
                     <h5 class="card-title text-light">' . $value['title'] . '</h5>
                     <p class="card-text text-light">' . $value['description'] . '</p>
--- header.php
+++ header.php
@@ -28,7 +28,9 @@ if (strpos($contact_page, $current_url) !== false) {
     <link rel="stylesheet" href="main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>WillyWonka</title>
+    <link rel="icon" type="image/png" href="image/favicon-16x16.png" />
+
 </head>
 <body>
     <header>
